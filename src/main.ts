77a9b4c5b05b83e179f6c42a1c2f9cc03f94146b
jonathaneverald <jonathaneverald@gmail.com>
2024-10-27 22:57:51 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,13 @@ web.listen(3000, () => {
   logger.info("Listening on port 3000");
 });
 
-web.use(
-  cors({
-    origin: ["http://localhost:3000", "your-frontend-production-url"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "X-API-TOKEN"],
-  })
-);
+// web.use(
+//   cors({
+//     origin: ["http://localhost:3000", "your-frontend-production-url"],
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+//     allowedHeaders: ["Content-Type", "X-API-TOKEN"],
+//   })
+// );
+
+web.use(cors());
