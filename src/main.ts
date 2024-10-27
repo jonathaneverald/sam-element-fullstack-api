60c4eafaa35984d35c0ebcd52709ba07a6e26964
jonathaneverald <jonathaneverald@gmail.com>
2024-10-27 23:36:40 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { logger } from "./application/logging";
 import { web } from "./application/web";
-import cors from "cors";
-import corsMiddleware from "./middleware/cors";
-import { NextFunction, Request, Response } from "express";
 
 web.listen(3000, () => {
   logger.info("Listening on port 3000");
--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,10 +1,10 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: true,
+  origin: "https://sam-element-fullstack-coding-challenge-frontend.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  allowedHeaders: ["Content-Type", "X-API-TOKEN"],
+  allowedHeaders: ["Content-Type", "X-API-TOKEN", "Authorization", "Accept"],
 });
 
 export default corsMiddleware;
