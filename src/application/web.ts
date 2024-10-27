e4c353d12565acb2b2bd5f2084331b1c8b66b8cc
jonathaneverald <jonathaneverald@gmail.com>
2024-10-27 23:41:03 +0700

    cors

--- src/application/web.ts
+++ src/application/web.ts
@@ -2,20 +2,9 @@ import express from "express";
 import { publicRouter } from "../route/public-api";
 import { errorMiddleware } from "../middleware/error-middleware";
 import { apiRouter } from "../route/api";
-import cors from "cors";
 import corsMiddleware from "../middleware/cors";
 
 export const web = express();
-
-web.use(
-  cors({
-    origin: true,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "X-API-TOKEN", "Authorization", "Accept"],
-    exposedHeaders: ["X-API-TOKEN"],
-  })
-);
 
 web.use(corsMiddleware);
 web.use(express.json());
--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,10 +1,11 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: "https://sam-element-fullstack-coding-challenge-frontend.vercel.app/",
+  origin: "https://sam-element-fullstack-coding-challenge-frontend.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "X-API-TOKEN", "Authorization", "Accept"],
+  exposedHeaders: ["X-API-TOKEN"],
 });
 
 export default corsMiddleware;
