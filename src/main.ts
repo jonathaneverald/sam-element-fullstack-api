fa4fe54cf0b393ded6e729b9a93ff07f76b9a3de
jonathaneverald <jonathaneverald@gmail.com>
2024-10-27 23:29:08 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,6 @@ import cors from "cors";
 import corsMiddleware from "./middleware/cors";
 import { NextFunction, Request, Response } from "express";
 
-web.use(corsMiddleware);
-
 web.listen(3000, () => {
   logger.info("Listening on port 3000");
 });
-
-web.use(
-  cors({
-    origin: true,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "X-API-TOKEN", "Authorization", "Accept"],
-    exposedHeaders: ["X-API-TOKEN"],
-  })
-);
