1aaf08144a9f6c8320165cb582630c06078fe596
jonathaneverald <jonathaneverald@gmail.com>
2024-10-27 23:07:55 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,19 @@ import { logger } from "./application/logging";
 import { web } from "./application/web";
 import cors from "cors";
 import corsMiddleware from "./middleware/cors";
+import { NextFunction, Request, Response } from "express";
 
-web.use(corsMiddleware);
+// web.use(corsMiddleware);
 
 web.listen(3000, () => {
   logger.info("Listening on port 3000");
 });
 
-// web.use(
-//   cors({
-//     origin: ["http://localhost:3000", "your-frontend-production-url"],
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-//     allowedHeaders: ["Content-Type", "X-API-TOKEN"],
-//   })
-// );
-
-web.use(cors());
+web.use(
+  cors({
+    origin: ["http://localhost:3000", "https://sam-element-fullstack-coding-challenge-frontend.vercel.app/"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "X-API-TOKEN"],
+  })
+);
--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: ["http://localhost:3000", "your-frontend-production-url"],
+  origin: ["http://localhost:3000", "https://sam-element-fullstack-coding-challenge-frontend.vercel.app/"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "X-API-TOKEN"],
